1a1e886147a340d147b6f4f9032d1d227f7218a2
Mustafa Elgohary <mustafaelgohary2000@gmail.com>
2025-01-10 05:39:59 +0200

    Update Header.js

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -129,21 +129,22 @@ const Header = () => {
                            </div>
                                </Link>
                                 </Fragment>)
-                                }else {
-                                    return (<Fragment>
-                                        <Link to={`/admin/order/{item.order._id}`} onClick={(e)=> readNotificationHandler(item._id) } className="dropdown-item">
-                                            <div key={item.order._id} className="row">
-                                             <div className="col-sm-12 col-md-2 d-flex justify-content-center">
-                                                    <img src={item.user.avatar.url} alt={item.user.name} style={{width: '50px', border: "1px solid black", height: '50px', borderRadius: "50%",margin: '15px'}} className="rounded-circle"></img>
-                                </div>
-               <div className="col-sm-12 col-md-10">
-                        <p key={item.order._id} style={{fontSize: '12px', color: 'black',textAlign:'right',fontWeight: item.isRead === true ?  "300" : "bold"}}>
-                                        أوردر جديد بواسطة :  <b>{item.user.name}</b>
-                                  </p>
-                               </div>
-                           </div>
-                               </Link>
-                                </Fragment>)}
+                                }
+            //                     else {
+            //                         return (<Fragment>
+            //                             <Link to={`/admin/order/${item.order._id}`} onClick={(e)=> readNotificationHandler(item._id) } className="dropdown-item">
+            //                                 <div key={item.order._id} className="row">
+            //                                  <div className="col-sm-12 col-md-2 d-flex justify-content-center">
+            //                                         <img src={item.user.avatar.url} alt={item.user.name} style={{width: '50px', border: "1px solid black", height: '50px', borderRadius: "50%",margin: '15px'}} className="rounded-circle"></img>
+            //                     </div>
+            //    <div className="col-sm-12 col-md-10">
+            //             <p key={item.order._id} style={{fontSize: '12px', color: 'black',textAlign:'right',fontWeight: item.isRead === true ?  "300" : "bold"}}>
+            //                             أوردر جديد بواسطة :  <b>{item.user.name}</b>
+            //                       </p>
+            //                    </div>
+            //                </div>
+            //                    </Link>
+            //                     </Fragment>)}
                                 
                             })}
                                </div>
