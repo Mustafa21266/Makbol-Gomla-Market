1bec4e9bbcb5ef0699974b01a926ed3b4b566679
Mustafa Elgohary <mustafaelgohary2000@gmail.com>
2025-01-10 04:44:30 +0200

    Update Header.js

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -116,7 +116,7 @@ const Header = () => {
                              {notifications && notifications.sort(function (a, b) { return a.isRead - b.isRead; }).map(item => (
                                <Fragment>
   <Link to={item.product ? `/product/${item.product._id}` : `/admin/order/${item.order._id}`} onClick={(e)=> readNotificationHandler(item._id) } className="dropdown-item">
-                <div key={item.order._id} className="row">
+                <div key={item.order ? item.order._id : item.product._id} className="row">
   {item.order && item.order._id && (
     <Fragment>
           <div className="col-sm-12 col-md-2 d-flex justify-content-center">
