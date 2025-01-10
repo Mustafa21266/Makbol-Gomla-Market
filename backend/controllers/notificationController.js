f5b6b5ffffccd49e63e7372a14cd44451b71bfef
Mustafa Elgohary <mustafaelgohary2000@gmail.com>
2025-01-10 22:34:43 +0200

    Update notificationController.js

--- backend/controllers/notificationController.js
+++ backend/controllers/notificationController.js
@@ -34,14 +34,7 @@ const cloudinary = require('cloudinary')
 
 //Get all notifications   =>     /api/v1/notifications
 exports.getNotifications = catchAsyncErrors(async (req, res, next) => {
-    const notifications = await Notification.find().populate('user', 'name email avatar').populate('order').populate({ 
-   path: 'user',
-      populate: {
-         path: 'User',
-         model: User,
-         select:"-_id name email avatar"
-   } 
- });
+    const notifications = await Notification.find().populate('user', 'name email avatar').populate('order');
     res.status(200).json({
         success: true,
         count: notifications.length,
