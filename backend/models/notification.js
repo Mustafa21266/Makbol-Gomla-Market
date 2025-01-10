b015fb7c79c6381cfc466b9c7c7acb44cbc2e470
Mustafa Elgohary <mustafaelgohary2000@gmail.com>
2025-01-10 02:50:59 +0200

    Update notification.js

--- backend/models/notification.js
+++ backend/models/notification.js
@@ -19,8 +19,13 @@ const notificationSchema = new mongoose.Schema({
     order: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Order',
-        required: true
+        required: false
     },
+    review: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: false
+    }
 })
 
 
