8f45502a8135efc9197955e2bb95f5d453524d0d
Mustafa Elgohary <mustafaelgohary2000@gmail.com>
2025-01-12 00:27:23 +0200

    adgadgsfgh

--- frontend/src/components/cart/Cart.js
+++ frontend/src/components/cart/Cart.js
@@ -197,10 +197,10 @@ const addToCart = (id) => {
                         }else {
                             return `${Number(item.price)}`
                         }
-                    }, 0)
-                        } 
-                        
-                        <span style={{marginLeft: '15px',marginRight: '15px'}}>EGP</span></span></p>
+                    }, 0)}
+                        <span style={{marginLeft: '15px',marginRight: '15px'}}>EGP</span> 
+                        </span>
+                        </p>
 </div>
                     </div>
                     <div className='row'>
@@ -208,7 +208,13 @@ const addToCart = (id) => {
                     <h4 style={{color: 'white', textAlign: 'center'}}>المجموع المحتمل  :  </h4>
 </div>
 <div className='col-4'>
-<p style={{textAlign: 'right'}}><span className="order-summary-values">{cartItems.reduce((acc, item)=> (acc + item.quantity * item.price), 0).toFixed(2)} EGP</span></p>
+<p style={{textAlign: 'right'}}>
+    
+    <span className="order-summary-values">
+        {cartItems.reduce((acc, item)=> (acc + item.quantity * item.price), 0).toFixed(2)}
+        </span>
+    <span style={{marginLeft: '15px',marginRight: '15px'}}>EGP</span>
+    </p>
 
 </div>
                     </div>
