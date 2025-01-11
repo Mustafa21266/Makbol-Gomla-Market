a38f125cb5d4e6f9534301c7f6e9ac6710af21b3
Mustafa Elgohary <mustafaelgohary2000@gmail.com>
2025-01-11 02:07:33 +0200

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -35,8 +35,8 @@ import 'animate.css';
 const options = {
   // you can also just use 'bottom center'
   position: positions.BOTTOM_CENTER,
-  timeout: 5000,
-  offset: '30px',
+  timeout: 60000,
+  offset: '80px',
   // you can also just use 'scale'
   transition: transitions.SCALE
 }
