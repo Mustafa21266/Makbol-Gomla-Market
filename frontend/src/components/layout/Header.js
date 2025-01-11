8f95d977da3eed4f64b7cb711b738123c3366735
Mustafa Elgohary <mustafaelgohary2000@gmail.com>
2025-01-11 21:44:02 +0200

    asfasdgadgadgasdgsdghdfgdnj

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -235,8 +235,6 @@ const Header = () => {
                              </div> 
                     
                   )}
-        </div>
-        <div className='col-12 col-md-6'>
         {user && user.role === 'user' && (
                            <div className="dropdown dropleft d-inline"> 
                            <Link to="#" style={{ textDecoration: 'none', backgroundColor:'#178a53' }} className="btn text-white" type="button" id="dropDownMenuButtonTwo" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -342,10 +340,8 @@ const Header = () => {
                              </div> 
                     
                   )}
-</div>
         </div>
-
-
+        <div className='col-12 col-md-6'>
        <div className="dropdown dropleft d-inline"> 
                 <Link to="/cart" style={{ textDecoration: 'none',backgroundColor:'#178a53' }} className="btn text-white" type="button" id="dropDownMenuButtonTwo" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <span className="ml-1" id="cart_count">{cartItems.length}</span>
@@ -383,6 +379,11 @@ const Header = () => {
                   </div>
                   </div> 
           </div> 
+</div>
+        </div>
+
+
+
           <div className='row'>
                     <div className='col-12'>
                         <Link to="/cart" style={{ textDecoration: 'none',backgroundColor:'#A49F48',textAlign: 'center' }} className="btn text-white w-100" type="button" id="dropDownMenuButtonTwo" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
