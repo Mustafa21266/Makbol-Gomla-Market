00a0e5c85a862db6e29368a891c72849345bba98
Mustafa Elgohary <mustafaelgohary2000@gmail.com>
2025-01-15 23:31:12 +0200

    Update GettingStarted.js

--- frontend/src/components/GettingStarted.js
+++ frontend/src/components/GettingStarted.js
@@ -21,10 +21,12 @@ let refr = null;
 const GettingStarted = ( { match } ) => {
   const dispatch = useDispatch();
   const alert = useAlert()
+  const [subcategory, setSubCategory] = useState('');
+  const [category, setCategory] = useState('');
   // const { loading, error, } = useSelector(state => state.products)
   // let keyword = window.location.href.includes("/search/all") ? match.params.keyword : 'home'
   // dispatch(getProducts(keyword,currentPage,price,category, subcategory, rating));
-  useEffect(() => {
+  useEffect(({ history }) => {
 
     
     return () => {
@@ -32,7 +34,14 @@ const GettingStarted = ( { match } ) => {
           window.location.reload();
         }
   }, [])
-  const switchView = () => {
+  const switchView = (page) => {
+    if(page === "home"){
+        history.push("/home")
+    }
+
+    if(page === "run"){
+        history.push(`/search/${subcategory.toLowerCase()}/${category.toLowerCase()}`)
+    }
     // dispatch({
     //   type: ALL_PRODUCTS_REQUEST,
     //   payload: []
@@ -41,23 +50,146 @@ const GettingStarted = ( { match } ) => {
     // setTimeout(function(){ window.location.reload(); }, 2000);
   }
     return (
+            
             <Fragment>
             <MetaData title={`مقبول جملة ماركت - Makboul Gomla Market`}/>
+            {!subcategory ? (
               <div>
                 <br />
                 <br />
     <div className="row">
     <div className="col-12 col-md-4 d-flex justify-content-center animate__animated animate__zoomIn animate__delay-2" style={{padding: '50px'}}>
-      <Link to={'/home'} className='getting-started-btn' onClick={()=> switchView()}>الرئيسية</Link>
+      <Link className='getting-started-btn' onClick={()=> switchView("home")}>الرئيسية</Link>
     </div>
     <div className="col-12 col-md-4 d-flex justify-content-center animate__animated animate__zoomIn animate__delay-4" style={{padding: '50px'}}>
-      <Link to={'/search/gomla'} className='getting-started-btn' onClick={()=> switchView()}>الجملة</Link>
+      <Link className='getting-started-btn' onClick={()=> 
+      {
+          setSubCategory("Gomla")
+        //   switchView("categories")
+      }
+        }>الجملة</Link>
     </div>
     <div className="col-12 col-md-4 d-flex justify-content-center animate__animated animate__zoomIn animate__delay-6" style={{padding: '50px'}}>
-      <Link to={'/search/piece'} className='getting-started-btn' onClick={()=> switchView()}>القطاعي</Link>
+      <Link className='getting-started-btn' onClick={()=> 
+      {
+          setSubCategory("Piece")
+        //   switchView("categories")
+      }
+        }>القطاعي</Link>
     </div>
 </div>
               </div>
+            ) : (
+                <div>
+                  <br />
+                  <br />
+      <div className="row">
+      <div className="col-12 col-md-4 d-flex justify-content-center animate__animated animate__zoomIn animate__delay-4" style={{padding: '50px'}}>
+        <Link className='getting-started-btn' onClick={()=> 
+        {
+            setCategory("Water")
+            switchView("run")
+        }
+          }>مياه</Link>
+      </div>
+      <div className="col-12 col-md-4 d-flex justify-content-center animate__animated animate__zoomIn animate__delay-6" style={{padding: '50px'}}>
+        <Link className='getting-started-btn' onClick={()=> 
+        {
+            setCategory("Soft Drinks")
+            switchView("run")
+        }
+          }>مشروبات غازية</Link>
+      </div>
+      <div className="col-12 col-md-4 d-flex justify-content-center animate__animated animate__zoomIn animate__delay-2" style={{padding: '50px'}}>
+        <Link className='getting-started-btn' onClick={()=> 
+        {
+            setCategory("Speciality Drinks")
+            switchView("run")
+        }
+          }>مشروبات مخصوصة</Link>
+      </div>
+  </div>
+  <div className="row">
+      <div className="col-12 col-md-4 d-flex justify-content-center animate__animated animate__zoomIn animate__delay-4" style={{padding: '50px'}}>
+        <Link className='getting-started-btn' onClick={()=> 
+        {
+            setCategory("Malt And Non-Alcholic")
+            switchView("run")
+        }
+          }>مشروبات الشعير وغير الكحوليات</Link>
+      </div>
+      <div className="col-12 col-md-4 d-flex justify-content-center animate__animated animate__zoomIn animate__delay-6" style={{padding: '50px'}}>
+        <Link className='getting-started-btn' onClick={()=> 
+        {
+            setCategory("Sports And Energy Drinks")
+            switchView("run")
+        }
+          }>مشروبات رياضية ومشروبات طاقة</Link>
+      </div>
+      <div className="col-12 col-md-4 d-flex justify-content-center animate__animated animate__zoomIn animate__delay-2" style={{padding: '50px'}}>
+        <Link className='getting-started-btn' onClick={()=> 
+        {
+            setCategory("Ice Cream")
+            switchView("run")
+        }
+          }>أيس كريم</Link>
+      </div>
+  </div>
+  <div className="row">
+      <div className="col-12 col-md-4 d-flex justify-content-center animate__animated animate__zoomIn animate__delay-4" style={{padding: '50px'}}>
+        <Link className='getting-started-btn' onClick={()=> 
+        {
+            setCategory("Chocolate")
+            switchView("run")
+        }
+          }>شوكلاتات</Link>
+      </div>
+      <div className="col-12 col-md-4 d-flex justify-content-center animate__animated animate__zoomIn animate__delay-6" style={{padding: '50px'}}>
+        <Link className='getting-started-btn' onClick={()=> 
+        {
+            setCategory("Candy And Gums")
+            switchView("run")
+        }
+          }>حلويات ومستيكة</Link>
+      </div>
+      <div className="col-12 col-md-4 d-flex justify-content-center animate__animated animate__zoomIn animate__delay-2" style={{padding: '50px'}}>
+        <Link className='getting-started-btn' onClick={()=> 
+        {
+            setCategory("Biscuits")
+            switchView("run")
+        }
+          }>بيسكويت</Link>
+      </div>
+  </div>
+  <div className="row">
+      <div className="col-12 col-md-4 d-flex justify-content-center animate__animated animate__zoomIn animate__delay-4" style={{padding: '50px'}}>
+        <Link className='getting-started-btn' onClick={()=> 
+        {
+            setCategory("Cakes")
+            switchView("run")
+        }
+          }>كيكات</Link>
+      </div>
+      <div className="col-12 col-md-4 d-flex justify-content-center animate__animated animate__zoomIn animate__delay-6" style={{padding: '50px'}}>
+        <Link className='getting-started-btn' onClick={()=> 
+        {
+            setCategory("Coffee And Tea")
+            switchView("run")
+        }
+          }>قهوة وشاي</Link>
+      </div>
+      <div className="col-12 col-md-4 d-flex justify-content-center animate__animated animate__zoomIn animate__delay-2" style={{padding: '50px'}}>
+        <Link className='getting-started-btn' onClick={()=> 
+        {
+            setCategory("Chips And Snacks")
+            switchView("run")
+        }
+          }>شيبسيهات وسناكس</Link>
+      </div>
+  </div>
+                </div>
+              )
+            }
         
 </Fragment>
     )
