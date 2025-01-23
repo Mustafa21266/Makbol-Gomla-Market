e8694b7eddc7372f2cff8b317fc220aea0c93aa9
Mustafa Elgohary <mustafaelgohary2000@gmail.com>
2025-01-23 03:45:48 +0200

    dhfgdfhfdhfhfhfhdfhd

--- frontend/src/components/product/Product.js
+++ frontend/src/components/product/Product.js
@@ -6,21 +6,26 @@ import { useAlert } from 'react-alert'
 const Product = ( { product, col } ) => {
       const [quantity, setQuantity] = useState(1)
       const dispatch = useDispatch();
-      const alert = useAlert()
-      const addToCart = (id) => {
-              dispatch(addItemToCart(id, quantity))
+      const alert = useAlert();
+      const { cartItems } = useSelector(state => state.cart)
+      const { loading , error, products } = useSelector(state => state.products);
+      const [prod_id, setProdId ] = useState("");
+      const decreaseQty = () => {
+        const count = document.querySelector('.count')
+        if(count.valueAsNumber <= 1) return;
+        const qty = count.valueAsNumber - 1;
+        setQuantity(qty);
+
+    }
+      const increaseQty = () => {
+              const count = document.querySelector('.count')
+              if(count.valueAsNumber >= product.stock) return;
+              const qty = count.valueAsNumber + 1;
+              setQuantity(qty);
+          }
+          const addToCart = () => {
+              dispatch(addItemToCart(product._id, quantity))
               alert.success('تم إضافة المنتج في سلة التسوق')
-          }
-          const decreaseQty = (id, quantity) => {
-              const newQty = quantity - 1;
-              if(newQty <= 0) return;
-              dispatch(addItemToCart(id, newQty))
-      
-          }
-          const increaseQty = (id, quantity, stock) => {
-              const newQty = quantity + 1;
-              if(newQty > stock) return;
-              dispatch(addItemToCart(id, newQty))
           }
     return (
         // <div className={`col-sm-10 col-md-6 col-lg-${col} my-3 d-inline-block mx-auto`}>
@@ -53,19 +58,16 @@ const Product = ( { product, col } ) => {
         <hr></hr>
                  <div className='row'>
                   <div className='col-12 w-100 d-block mx-auto'>
-                  <div className="stockCounter w-100 d-flex justify-content-center mx-auto">
-                    {/* <span className="btn btn-danger minus" style={{padding: '10px 20px'}} onClick={decreaseQty}>-</span>
+                  <div className="stockCounter d-inline">
+                    <span className="btn btn-danger minus" style={{padding: '10px 20px'}} onClick={decreaseQty}>-</span>
 
                     <input type="number" className="form-control count d-inline" value={quantity} readOnly />
 
-                    <span className="btn btn-primary plus" style={{padding: '10px 20px'}} onClick={increaseQty}>+</span> */}
-                                                    <span className="btn minus"  style={{padding: '10px 20px',backgroundColor:'red'}}  onClick={ () => decreaseQty(product._id, product.quantity)}>-</span>
-                                <input type="number" className="form-control count d-inline" value={quantity} readOnly />
-
-								<span className="btn btn-primary plus" style={{padding: '10px 20px'}}  onClick={() => increaseQty(product._id, product.quantity, product.stock)}>+</span>
+                    <span className="btn btn-primary plus" style={{padding: '10px 20px'}} onClick={increaseQty}>+</span>
                 </div>
-                 <button type="button" id="cart_btn" className="btn d-block mx-auto" disabled={product.stock === 0}  style={{backgroundColor:'#178a53', color: 'white'}}  onClick={addToCart}>إضافة إلى السلة</button>
+                 <button type="button" id="cart_btn" className="btn d-inline ml-4" disabled={product.stock === 0}  style={{backgroundColor:'#178a53', color: 'white'}}  onClick={addToCart}>إضافة إلى السلة</button>
 
+                 <hr></hr>
                   </div>
                  </div>
     </div>
