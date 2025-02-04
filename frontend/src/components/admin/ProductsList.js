d90f5398eac7c82ac6958207b6542472e3638e2d
Mustafa Elgohary <mustafaelgohary2000@gmail.com>
2025-02-05 01:04:52 +0200

    شسبشسيبشسببشش

--- frontend/src/components/admin/ProductsList.js
+++ frontend/src/components/admin/ProductsList.js
@@ -46,7 +46,7 @@ const ProductsList = ({ history }) => {
             columns: [
                 {
                     label: 'الصنف',
-                    field: 'category',
+                    field: 'subcategory',
                     sort: 'asc'
                 },
                 {
@@ -82,7 +82,7 @@ const ProductsList = ({ history }) => {
             .forEach(product => {
             data.rows = data.rows.concat ({
                 id: product._id,
-                category: product.category,
+                category: product.subcategory === "Gomla" ? "جملة" : "قطاعي",
                 name: product.name,
                 picture: <img src={product.images[0].url} style={{width: '100px',height:'100px'}} />,
                 price: `${product.price} EGP`,
